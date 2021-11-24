726b8d81103fd4223b2502f4cd9d37cf5b19efbc
Usharani8 <94221177+Usharani8@users.noreply.github.com>
2021-11-24 09:51:56 +0530

    Update paper.c

--- 3_Implementation/test/paper.c
+++ 3_Implementation/test/paper.c
@@ -1,29 +1,25 @@
 
-#include"rps.h" // Function to implement the game int game(char you, char computer) { // If both the user and computer // has chose the same thing if (you == computer) { return 0; }
-
-// If user's choice is Rock and
-
-// computer's choice is Paper
-
-if (you == 'r' && computer == 'p')
-
+#include"rps.h"
+// Function to implement the game
+int game(char you, char computer)
 {
+ // If both the user and computer // has chose the same thing
+ case1: (you == computer) 
+ {
+  return 0;
+ }
 
+// If user's choice is Rock and computer's choice is Paper
+case2: (you == 'r' && computer == 'p')
+{
 return -1; 
-
 }
 
- // If user's choice is Paper and
- 
-// computer's choice is Rock
-
-if (you == 'p' && computer == 'r')
-
-  {
-  
-        return 1;
-        
-  }
+ // If user's choice is Paper and computer's choice is Rock
+case3: (you == 'p' && computer == 'r')
+{
+  return 1;
+}
   
 
 // If user's choice is Rock and
@@ -64,17 +60,10 @@ if (you == 'p' && computer == 'z')
 }
 
 
-// If user's choice is Scissor and
-
-// computer's choice is Paper
-
-if (you == 'z' && computer == 'p')
-
+// If user's choice is Scissor and computer's choice is Paper
+case7:(you == 'z' && computer == 'p')
 {
-
-    return 1;
-    
+    return 1;    
 }
-
 }
 
